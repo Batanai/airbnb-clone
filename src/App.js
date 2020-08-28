4dced02f20f4f155656aaafe4cb7e7627e4d2e73
Batanai <31771294+Batanai@users.noreply.github.com>
2020-08-28 13:17:11 +0200

    completeCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./navbar/Navbar";
+import Home from "./home/Home";
+import Footer from "./footer/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./searchPage/SearchPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/home/banner/datepicker/Datepicker.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react'
+import './Datepicker.css'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import PeopleIcon from "@material-ui/icons/People";
+import { Button } from "@material-ui/core";
+import { useHistory } from 'react-router-dom';
+
+const Datepicker = () => {
+    const history = useHistory();
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const selectionRange = {
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection',
+      };
+
+      const handleSelect = (ranges) => {
+          setStartDate(ranges.selection.startDate);
+          setEndDate(ranges.selection.endDate);
+      }
+    return (
+        <div className="datepicker">
+            <DateRangePicker
+            ranges={[selectionRange]}
+            onChange={handleSelect}
+            />
+
+            <h2>
+                Number of guests
+                <PeopleIcon/>
+            </h2>
+
+            <input
+            min={0}
+            defaultValue={2}
+            type="number" 
+            />
+
+            <Button onClick={() => history.push("/search")}>Search Airbnb</Button>
+        </div>
+    )
+}
+
+export default Datepicker
